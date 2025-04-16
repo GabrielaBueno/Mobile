6e858713e0a7cae8b99a3147fd6fbc9c115ba156
Gabriela Bueno <68259978+GabrielaBueno@users.noreply.github.com>
2025-04-16 20:51:27 -0300

    Add files via upload

---
+++ lib/ui/_core/app_colors.dart
@@ -0,0 +1,8 @@
+// Condensa as cores do app
+import 'package:flutter/widgets.dart';
+
+abstract class AppColors {
+  static const Color backgroundColor = Color(0xFF202123);
+  static const Color mainColor = Color(0xFFFFA559);
+  static const Color lightBackgroundColor = Color(0xFF343541);
+}
